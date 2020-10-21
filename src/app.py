2c2ac38f9eb50c910239398f5cd95daa1f816fe7
Eric Lofaso <elofaso@pm.me>
2020-10-21 19:39:10 +0000

    improved exception handling. added comments.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
 import json
 import boto3
+import botocore
 
 # Grab data from the environment.
 AWS_REGION = os.getenv('AWS_REGION', 'us-east-1')
@@ -12,14 +13,16 @@ def lambda_handler(event, context):
     
     visit_count = None
 
+    # Try to get current value of visit_count from db.
     try:
-        response = table.get_item(Key={
+        response = table.get_item(
             Key={
                 'Id': 1
             }            
         )
         old_visit_count = int(response['Item']['visit_count'])
-    except Exception:
+    # Create and initialize visit_count in db if item does not exist.
+    except KeyError:
         table.put_item(
             Item={
                     'Id':  1,
@@ -27,9 +30,11 @@ def lambda_handler(event, context):
             }
         )
         old_visit_count = 0
-        
+    
+    # Add 1 to count for current visit.   
     new_visit_count = old_visit_count + 1
     
+    # Update visit_count in db to new_vist_count
     table.update_item(
         Key={'Id': 1},
         UpdateExpression="set visit_count= :vc",
@@ -38,9 +43,8 @@ def lambda_handler(event, context):
         },
     )
     
+    # Return success status code and value of new_visit_count.
     return {
         "statusCode": 200,
         "body": json.dumps(new_visit_count)
     }
-    
-    
